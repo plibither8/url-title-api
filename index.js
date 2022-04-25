d5abecfca079b29d7201fbd954ffff4fb6f01f2e
Mihir Chaturvedi <plibither8@gmail.com>
2022-04-25 09:28:53 +0530

    test: Analytics

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-const fastify = require('fastify')()
-const fetch = require('node-fetch')
+const fastify = require("fastify")();
+const fetch = require("node-fetch");
 
-fastify.register(require('fastify-url-data'))
+fastify.register(require("fastify-url-data"));
 
-fastify.get('/*', async (req, reply) => {
-	const { path } = req.urlData()
-	const url = 'http://' + decodeURIComponent(path).substring(1)
-	const rawText = await fetch(url)
-		.then(res => res.text())
-		.catch(err => {
-			if (err) {
-				reply
-					.code(400)
-					.send({
-						statusCode: 400,
-						message: 'BAD REQUEST: ' + err.message
-					})
-			}
-		})
+fastify.get("/*", async (req, reply) => {
+  const { headers } = req;
+  const ip =
+    headers["cf-connecting-ip"] ||
+    headers["x-real-ip"] ||
+    headers["x-forwarded-for"];
+  console.log(headers.referer, ip);
+  const { path } = req.urlData();
+  const url = "http://" + decodeURIComponent(path).substring(1);
+  const rawText = await fetch(url)
+    .then((res) => res.text())
+    .catch((err) => {
+      if (err) {
+        reply.code(400).send({
+          statusCode: 400,
+          message: "BAD REQUEST: " + err.message,
+        });
+      }
+    });
 
-	const matches = rawText.match(/<title>(.+)<\/title>/)
-	const title = matches ? matches[1] : ''
+  const matches = rawText.match(/<title>(.+)<\/title>/);
+  const title = matches ? matches[1] : "";
 
-	reply.send(title)
-})
+  reply.send(title);
+});
 
-fastify.listen(process.env.PORT || 3000, err => {
-	if (err) throw err
-})
+fastify.listen(process.env.PORT || 3000, (err) => {
+  if (err) throw err;
+});
