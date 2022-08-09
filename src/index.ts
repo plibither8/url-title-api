49ae3b33301a0db09301de4d82ebd4d927e1c212
Mihir Chaturvedi <plibither8@gmail.com>
2022-08-09 17:47:06 +0530

    refactor: New worker syntax

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
-async function handleRequest(request: Request): Promise<Response> {
-  const path = new URL(request.url).pathname;
-  let requestedUrl: string = path.substring(1);
+import { Router } from "itty-router";
+
+const router = Router();
+
+router.get("*", async (request) => {
+  const { pathname } = new URL(request.url);
+  let requestedUrl: string = pathname.substring(1);
   ["http:/", "https:/"].forEach((prefix) => {
     if (requestedUrl.startsWith(prefix))
       requestedUrl = requestedUrl.substring(prefix.length);
   });
   requestedUrl = `http://${requestedUrl}`;
+
   try {
     const url = new URL(requestedUrl);
     const html = await fetch(url.href).then((res) => res.text());
     const matches = html.match(/<title>(.*?)<\/title>/);
     return new Response(matches?.[1] ?? "");
   } catch (err: any) {
-    return new Response(`Error: ${err.message}`, {
-      status: 500,
-      headers: { "Content-Type": "text/plain" },
-    });
+    return new Response(`Error: ${err.message}`, { status: 500 });
   }
-}
-
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
 });
+
+export default {
+  async fetch(request: Request): Promise<Response> {
+    return router.handle(request);
+  },
+};
